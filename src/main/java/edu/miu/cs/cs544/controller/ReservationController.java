8ed8110b9d8822f0e2bfaae9fe6dcb8f187b4fe6
EphremG <fstudioceh@gmail.com>
2023-12-16 16:00:51 -0600

    initial update for the reservations

--- src/main/java/edu/miu/cs/cs544/controller/ReservationController.java
+++ src/main/java/edu/miu/cs/cs544/controller/ReservationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reservations")
+@RequestMapping("/admin/reservations")
 public class ReservationController {
 
     @Autowired
