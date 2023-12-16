6326f0740f27a7175e52c66e554ec4897b6ec5ec
alexchang <long.chang@live.com>
2023-12-16 15:08:52 -0600

    initial update for the reservation

---
+++ src/main/java/edu/miu/cs/cs544/controller/ReservationController.java
@@ -0,0 +1,40 @@
+package edu.miu.cs.cs544.controller;
+
+import edu.miu.cs.cs544.domain.Reservation;
+import edu.miu.cs.cs544.dto.ReservationDto;
+import edu.miu.cs.cs544.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getAllReservations() {
+        return ResponseEntity.ok(reservationService.getAllReservations());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> getReservationById(@PathVariable Integer id) {
+        return ResponseEntity.ok(reservationService.getReservationById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> createReservation(@RequestBody ReservationDto reservationDto) {
+        // convert dto to entity and save
+        return ResponseEntity.ok(reservationService.createReservation(new Reservation()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Integer id) {
+        reservationService.deleteReservation(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/edu/miu/cs/cs544/service/ReservationService.java
@@ -0,0 +1,11 @@
+package edu.miu.cs.cs544.service;
+
+import edu.miu.cs.cs544.domain.Reservation;
+import java.util.List;
+
+public interface ReservationService {
+    List<Reservation> getAllReservations();
+    Reservation getReservationById(Integer id);
+    Reservation createReservation(Reservation reservation);
+    void deleteReservation(Integer id);
+}
