4a2a13809fcd9240b2e63575606202d2e077877e
ATeshome <71078949+AlemuTeshome@users.noreply.github.com>
2023-12-19 11:07:30 -0600

    Application class updated: 10:48AM

--- src/main/java/edu/miu/cs/cs544/Application.java
+++ src/main/java/edu/miu/cs/cs544/Application.java
@@ -5,29 +5,23 @@ import edu.miu.cs.cs544.dto.LoginRequest;
 import edu.miu.cs.cs544.repository.CustomerRepository;
 import edu.miu.cs.cs544.repository.ProductRepository;
 import edu.miu.cs.cs544.repository.UserRepository;
-import edu.miu.cs.cs544.service.CustomerService;
 import edu.miu.cs.cs544.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @SpringBootApplication
 @EntityScan("edu.miu.cs.cs544.domain")
 @EnableJpaRepositories("edu.miu.cs.cs544.repository")
 public class Application implements CommandLineRunner {
-
     @Autowired
-    UserRepository repository;
+    UserRepository userRepository;
 
     @Autowired
     ProductRepository productRepository;
@@ -39,7 +33,7 @@ public class Application implements CommandLineRunner {
     LoginService loginService;
     @Autowired
     CustomerRepository customerRepository;
-    static ApplicationContext context;
+   static ApplicationContext context;
 
     public static void main(String[] args) {
 
@@ -47,26 +41,47 @@ public class Application implements CommandLineRunner {
     }
 
     @Override
-    @Transactional
     public void run(String... args) throws Exception {
 
+
+
+        Country country = new Country();
+        country.setCode("US");
+        country.setName("America");
+        country.setPopulation(10000);
+
+        State state = new State();
+        state.setCode("IA");
+        state.setCountry(country);
+
         Address physicalAddress = new Address();
-        physicalAddress.setCity("City1");
+        physicalAddress.setCity("Fairfield");
         physicalAddress.setType(AddressType.PHYSICAL);
+        physicalAddress.setState(state);
+
         Address billing = new Address();
-        billing.setCity("City1");
+        billing.setCity("Fairfield");
         billing.setType(AddressType.BILLING);
+        billing.setState(state);
+        billing.setLine1("line1");
+        billing.setPostalCode("44444");
+
 
         Customer customer = new Customer();
-        customer.setEmail("sample@email.com");
-        customer.setFirstName("firstName");
-        customer.setLastName("Lastname");
+        customer.setEmail("alemu@email.com");
+        customer.setFirstName("Alemu");
+        customer.setLastName("Teshome");
         customer.setPhysicalAddress(physicalAddress);
         customer.setBillingAddress(billing);
 
+        User user = new User("AlemuTeshome",encoder.encode("123456789"),true, UserType.CUSTOMER, null);
+        customer.setUser(user);
         customerRepository.save(customer);
+        //userRepository.save(user);
+        var response = loginService.login(new LoginRequest("AlemuTeshome","123456789"));
+        System.out.println(response);
 
         //Create Product 1
-        productRepository.save(Product.builder().name("Product1").maxCapacity(3).rate(250.0).build());
+        //productRepository.save(Product.builder().name("Product1").maxCapacity(3).rate(250.0).build());
     }
 }
