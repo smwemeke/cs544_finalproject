eb50e859f13e1de133c6ecba93fe4784139964b0
Yenatfanta <yfeyyisa@miu.edu>
2023-12-19 13:00:54 -0600

    ProductServiceTest updates

--- src/test/java/edu/miu/cs/cs544/service/ProductServiceTest.java
+++ src/test/java/edu/miu/cs/cs544/service/ProductServiceTest.java
@@ -2,7 +2,10 @@ package edu.miu.cs.cs544.service;
 
 import edu.miu.cs.cs544.domain.Product;
 import edu.miu.cs.cs544.domain.ProductType;
+import edu.miu.cs.cs544.dto.ProductResponse;
+import edu.miu.cs.cs544.repository.CustomerRepository;
 import edu.miu.cs.cs544.repository.ProductRepository;
+import edu.miu.cs.cs544.repository.UserRepository;
 import edu.miu.cs.cs544.service.ProductService;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -22,25 +26,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ProductServiceTest {
-    @Autowired
+    @MockBean
     private ProductService productService;
     @MockBean
     private ProductRepository productRepository;
     @Autowired
     TestEntityManager entityManager;
+    @MockBean
+    CustomerRepository customerRepository;
+    @MockBean
+    LoginService loginService;
+    @MockBean
+    PasswordEncoder encoder;
+    @MockBean
+    UserRepository userRepository;
+
 
     @Before
     public void setUp(){
         int id = 1;
-        Product product = new Product( id,"TestProduct", "Description", "Excerpt", 10.0, 100, ProductType.Room,true);
-        Optional<Product> optionalProduct = Optional.of(product);
-        Mockito.when(productRepository.findById(id)).thenReturn(product);
+
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setId(id);
+
+        Mockito.when(productService.getProduct(id)).thenReturn(productResponse);
     }
 
     @Test
-    public void testCreateProduct(){
+    public void testGetProduct(){
        int id = 1;
-        Product created = new Product( id,"TestProduct", "Description", "Excerpt", 10.0, 100, ProductType.Room,true);
-        assertThat(created.getId()).isEqualTo(id);
+        ProductResponse found = productService.getProduct(id);
+        assertThat(found.getId()).isEqualTo(id);
     }
+
 }
